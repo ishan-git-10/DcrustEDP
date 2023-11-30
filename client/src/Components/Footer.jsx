b4d4a911b70f2d0c1f9d1d71bc10c7a484389870
ishan-git-10 <ishan06010@gmail.com>
2023-11-30 13:22:55 +0530

    Home Screen added

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import {Row, Col, Container, Image } from 'react-bootstrap';
+import { FaTwitter } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { FaFacebookF } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
-function HomeScreen() {
-    return (
-        <div>Hello</div>
+function Footer() {
+    let currTime = new Date().getFullYear();
+    return(
+        <footer className="bg-dark text-light pt-5 mt-5">
+        <Container>
+            <Row>
+                <Col md={6} className="d-flex justify-content-center">
+                    <Image src={require("./images/ulogo.png")} className="logo-img-footer"></Image>
+                    <h5 className='brand-div mt-2'>DCRUST, Murthal
+                    <br />
+                    <a href="https://twitter.com/MurthalDcrust" target="_blank" rel="noreferrer" className="social-link"><FaTwitter /> </a>
+                    <a href="mailto:dcrustmurthal.edpcentre@gmail.com" target="_blank" rel="noreferrer" className="social-link"><IoMdMail /> </a>
+                    <a href="https://www.facebook.com/officialdcrustm" target="_blank" rel="noreferrer" className="social-link"><FaFacebookF /> </a>
+                    <br />
+                    <span className="social-link">+91-130-2484095</span>
+                    </h5>
+                </Col>
+                <Col md={6}>
+                    <p className="pt-2 text-center">Designed and developed by
+                    <br /> 
+                    Ekta and Ishan.
+                    </p>
+                </Col>
+            </Row>
+            <Row className="mt-3">
+                <Col>
+                    <hr className="border-light" />
+                    <p className="text-center">Dcrust EDP ⓒ {currTime}</p>
+                </Col>
+            </Row>
+        </Container>
+    </footer>
     );
 }
 
-export default HomeScreen;
+export default Footer;
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,9 +1,97 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Navbar, Nav, Container, NavDropdown, Image, Row, Col } from 'react-bootstrap';
+import {LinkContainer} from "react-router-bootstrap";
+import { IoPerson } from "react-icons/io5";
+import {Link} from "react-router-dom";
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useLogoutMutation } from '../slices/usersApiSlice';
+// import { useNavigate } from 'react-router-dom';
+// import { logout } from '../slices/authSlice';
+import "./Header.css";
 
-function HomeScreen() {
-    return (
-        <div>Hello</div>
-    );
+
+function Header() {
+
+//   const { userInfo } = useSelector((state) => state.auth);
+
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const [logoutApiCall] = useLogoutMutation();
+
+//   const logoutHandler = async () => {
+//     try {
+//       await logoutApiCall().unwrap();
+//       dispatch(logout());
+//       navigate('/login');
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+  return (
+    <div className="bg-primary heading" data-bs-theme="dark">
+        <Container>
+        <Row >
+                <Col lg className='header-div-center'>
+                <Link to="/"><Image src={require("./images/ulogo.png")} className="logo-img"></Image>
+                    <div className='brand-div'>
+                        <h5 className='brand'>Deenbandhu Chhotu Ram University of Science and Technology</h5>
+                        <br />
+                        <h6 className='tag'>Murthal, Sonipat, Haryana, India (A Haryana State Government NAAC accredited University)</h6>
+                    </div>
+                    </Link>
+                </Col>
+        </Row>
+        <Row>
+        <Navbar collapseOnSelect expand="lg" className='justify-content-center'>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-center' >
+          <Nav className='nav-links'>
+            <LinkContainer to='/about'>
+              <Nav.Link className="text-center nav-link">About</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/institutions'>
+              <Nav.Link className="text-center nav-link">Institutions</Nav.Link>
+            </LinkContainer>
+            <NavDropdown className="text-center" title="Programmes" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3" >
+                PHD Programmes
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                PG Programmes
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">
+                Dual Degree Programmes
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">
+                UG Programmes
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">
+                PG Diploma Programmes
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">
+                Certificate Programmes
+              </NavDropdown.Item>
+            </NavDropdown>
+            <LinkContainer to='/rules'>
+              <Nav.Link className="text-center nav-link">Rulebook</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/contact'>
+              <Nav.Link className="text-center nav-link">Contact</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+              <Nav.Link className="text-center nav-link"><IoPerson className='mb-1'/> Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          
+        </Navbar.Collapse>
+        </Navbar>
+        </Row>
+        </Container>
+    </div>
+        
+  );
 }
 
-export default HomeScreen;
+export default Header;
