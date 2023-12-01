0b85c7c0b18b9472efc4fde02eab3ca7a138beb2
ishan-git-10 <ishan06010@gmail.com>
2023-12-01 09:48:37 +0530

    register controller updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,17 +27,16 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { username, password } = req.body;
 
-  const userExists = await User.findOne({ email });
+  const userExists = await User.findOne({ username });
 
   if (userExists) {
     res.status(400).json({ message: "User already exists" });
   }
 
   const user = await User.create({
-    name,
-    email,
+    username,
     password,
   });
 
@@ -46,8 +45,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     res.status(201).json({
       _id: user._id,
-      name: user.name,
-      email: user.email,
+      username: user.username,
       isAdmin: user.isAdmin,
     });
   } else {
